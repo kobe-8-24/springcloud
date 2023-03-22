07d593bb8082c83a45acc2f171efed7078913a0a
dickinson <576207068@qq.com>
2023-03-22 13:16:02 +0800

    skywalking稳定版本 2023-03-22 13:15:00

--- cache-service/src/main/java/com/htsc/CacheApplication.java
+++ cache-service/src/main/java/com/htsc/CacheApplication.java
@@ -1,11 +1,29 @@
 package com.htsc;
 
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class CacheApplication {
     public static void main(String[] args) {
         SpringApplication.run(CacheApplication.class);
+    }
+
+    @Bean
+    public ElasticsearchClient elasticsearchClient() {
+        return new ElasticsearchClient(new RestClientTransport(RestClient.builder(new HttpHost("127.0.0.1",
+                9200)).build(), new JacksonJsonpMapper()));
+    }
+
+    @Bean
+    public RestTemplate get() {
+        return new RestTemplate();
     }
 }
